4a70c332275b778adf8310644e7e2eec3b7586d1
Brian Jaress <bjaress@gmail.com>
2024-04-10 13:51:10 -1000

    listing languages

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
-set -ex
+set -e
 
 language="$1"
 dir="$(dirname "$(realpath "$0")")"
 iidfile="$(mktemp)"
+
+if [ "$#" -eq 0 ]
+then
+  find "${dir}" -iname Dockerfile -printf "%P\n"|xargs dirname|sort
+  exit
+fi
 
 podman build --iidfile "${iidfile}" "${dir}/${language}"
 exec podman run -it "$(< "${iidfile}" )"
