e1d6f422fed40e34cfdbfdcf83f351753a2ee1ea
Brian Jaress <Brian.Jaress@modahealth.com>
2024-04-10 07:49:11 -1000

    script fixes, more languages

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 set -ex
 
 language="$1"
-dir="$(realpath "$(dirname "$0")")"
-iidfile=$(mktemp)
+dir="$(dirname "$(realpath "$0")")"
+iidfile="$(mktemp)"
 
 podman build --iidfile "${iidfile}" "${dir}/${language}"
 exec podman run -it "$(< "${iidfile}" )"
